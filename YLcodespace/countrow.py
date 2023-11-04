842bac5dc1ad388d4eccbc9d60cc11fd65826807
Thenononshy <youl19@uci.edu>
2023-11-03 17:02:10 -0700

    sponsor_data file

--- YLcodespace/countrow.py
+++ YLcodespace/countrow.py
@@ -2,7 +2,7 @@
 import csv
 
 
-file_path = '/Users/youli/Desktop/UC_Irvine/FQ_2023/DS-CapStone-Project-Group9/Analysis CSVs/merged3files.csv'
+file_path = '/Users/youli/Desktop/UC_Irvine/FQ_2023/DS-CapStone-Project-Group9/sponsor_data/redacted_full_stories_table.csv'
 
 
 with open(file_path, 'r', encoding='utf-8') as file:
