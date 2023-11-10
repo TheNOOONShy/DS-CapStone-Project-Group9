2411f3fd1ead085d5f5d550f9db18e92cb5a6f68
Tim Huang <readatontim@gmail.com>
2023-11-10 09:19:16 -0800

    updated

--- process_code/file_lenghts.py
+++ process_code/file_lenghts.py
@@ -6,42 +6,43 @@ def custom_split(content):
     return content.replace("</br></br>", "\n").split("\n")
 
 def process_file(file_path):
+    print(f"Processing file: {file_path}")
     with open(file_path, 'r', encoding='utf-8', errors='replace') as file:
         content = file.read()
 
     if "UCI:" in content and "Caller:" in content:
-        file_type = "call"
+        is_call = 1
         lines = custom_split(content)
         relevant_lines = [line for line in lines if "Caller:" in line]
         length = sum(len(line) for line in relevant_lines)
     else:
-        file_type = "letter"
+        is_call = 0
         length = len(content)
 
     return {
         'file_name': os.path.basename(file_path),
-        'call/letter': file_type,
+        'is_call': is_call,
         'length': length
     }
 
 def process_docx(file_path):
+    print(f"Processing docx: {file_path}")
     with open(file_path, 'rb') as file:
         result = mammoth.extract_raw_text(file)
         content = result.value.encode('utf-8', errors='replace').decode('utf-8')
 
-
     if "UCI:" in content and "Caller:" in content:
-        file_type = "call"
+        is_call = 1
         lines = custom_split(content)
         relevant_lines = [line for line in lines if "Caller:" in line]
         length = sum(len(line) for line in relevant_lines)
     else:
-        file_type = "letter"
+        is_call = 0
         length = len(content)
 
     return {
         'file_name': os.path.basename(file_path),
-        'call/letter': file_type,
+        'is_call': is_call,
         'length': length
     }
 
@@ -49,7 +50,7 @@ def main():
     output_file = 'C:\\Users\\reada\\github\\DS-CapStone-Project-Group9\\output.csv'
 
     with open(output_file, 'w', newline='') as csvfile:
-        fieldnames = ['file_name', 'call/letter', 'length']
+        fieldnames = ['file_name', 'is_call', 'length']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
 
@@ -57,8 +58,6 @@ def main():
 
         for filename in os.listdir(base_folder):
             file_path = os.path.join(base_folder, filename)
-
-            print(f"Running {filename}")
 
             if filename.endswith('.txt'):
                 data = process_file(file_path)
