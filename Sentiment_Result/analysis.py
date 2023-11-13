dbf41f0c5ccbad2ffe7c5418023734777e638060
Tim Huang <readatontim@gmail.com>
2023-11-13 15:39:53 -0800

    more analysis

--- Sentiment_Result/analysis.py
+++ Sentiment_Result/analysis.py
@@ -5,20 +5,27 @@ import statsmodels.api as sm
 # Load the merged data
 merged_df = pd.read_csv('final_merged_data.csv')
 
+
+
 # Convert the 'Type' column to dummy variables
 merged_df = pd.get_dummies(merged_df, columns=['Type'], drop_first=True)
 
-# Create the combined variable 'Case_Death_Number'
-merged_df['Case_Death_Number'] = merged_df['Prison Case Number'] + merged_df['Prison Death Number']
+# Exclude rows where 'Type' is 'Jail'
+merged_df = merged_df[merged_df['Type_Jail'] == 0]
+
+# Separate 'Case_Death_Number' into 'Number of Cases' and 'Number of Deaths'
+# Separate 'Case_Death_Number' into 'Number of Cases' and 'Number of Deaths'
+merged_df['Number of Cases'] = merged_df['Prison Case Number']
+merged_df['Number of Deaths'] = merged_df['Prison Death Number']
 
 # Include the 'Level' variable
 merged_df = pd.get_dummies(merged_df, columns=['Level'], drop_first=True)
 
 # Rename columns to adjust the naming conventions
-merged_df.rename(columns={'Type_jail': 'Type_Jail', 'Positive_count': 'Positive_Count'}, inplace=True)
+merged_df.rename(columns={'Positive_count': 'Positive_Count'}, inplace=True)
 
 # Define the independent variables (features) for Positive_Count
-independent_vars_positive = ['Week Number', 'Overall Count_x', 'Type_Jail', 'Case_Death_Number', 'Level_State', 'Level_Federal']
+independent_vars_positive = ['Week Number', 'Overall Count_x', 'Number of Cases', 'Number of Deaths', 'Level_State']
 
 # Add a constant term for the intercept
 X_positive = sm.add_constant(merged_df[independent_vars_positive])
@@ -29,17 +36,17 @@ y_positive = merged_df['Positive_Count']
 # Fit the linear regression model for Positive_Count
 model_positive = sm.OLS(y_positive, X_positive).fit()
 
-# Save the model to a file within the "flair_regression" folder
+# Save the model to a file within the "flair_regression" folder for prisons only
 os.makedirs('flair_regression', exist_ok=True)
-filename_positive = '_'.join(independent_vars_positive)
-model_positive.save(os.path.join('flair_regression', f'positive_count_model_{filename_positive}.pkl'))
+filename_positive_prisons = '_'.join(independent_vars_positive + ['Prisons'])
+model_positive.save(os.path.join('flair_regression', f'positive_count_model_{filename_positive_prisons}.pkl'))
 
 # Print the summary of the regression for Positive_Count
-print(f"Linear Regression for Positive_Count using independent variables: {', '.join(independent_vars_positive)}")
+print(f"Linear Regression for Positive_Count (Prisons only) using independent variables: {', '.join(independent_vars_positive)}")
 print(model_positive.summary())
 
 # Define the independent variables (features) for Negative_Count
-independent_vars_negative = ['Week Number', 'Overall Count_x', 'Type_Jail', 'Case_Death_Number', 'Level_State', 'Level_Federal']
+independent_vars_negative = ['Week Number', 'Overall Count_x', 'Number of Cases', 'Number of Deaths', 'Level_State']
 
 # Add a constant term for the intercept
 X_negative = sm.add_constant(merged_df[independent_vars_negative])
@@ -50,16 +57,16 @@ y_negative = merged_df['Negative_Count']
 # Fit the linear regression model for Negative_Count
 model_negative = sm.OLS(y_negative, X_negative).fit()
 
-# Save the model to a file within the "flair_regression" folder
-filename_negative = '_'.join(independent_vars_negative)
-model_negative.save(os.path.join('flair_regression', f'negative_count_model_{filename_negative}.pkl'))
+# Save the model to a file within the "flair_regression" folder for prisons only
+filename_negative_prisons = '_'.join(independent_vars_negative + ['Prisons'])
+model_negative.save(os.path.join('flair_regression', f'negative_count_model_{filename_negative_prisons}.pkl'))
 
 # Print the summary of the regression for Negative_Count
-print(f"\nLinear Regression for Negative_Count using independent variables: {', '.join(independent_vars_negative)}")
+print(f"\nLinear Regression for Negative_Count (Prisons only) using independent variables: {', '.join(independent_vars_negative)}")
 print(model_negative.summary())
 
 # Define the independent variables (features) for Total_Count
-independent_vars_total_count = ['Week Number', 'Overall Count_x', 'Type_Jail', 'Case_Death_Number', 'Level_State', 'Level_Federal']
+independent_vars_total_count = ['Week Number', 'Overall Count_x', 'Number of Cases', 'Number of Deaths', 'Level_State']
 
 # Add a constant term for the intercept
 X_total_count = sm.add_constant(merged_df[independent_vars_total_count])
@@ -70,10 +77,10 @@ y_total_count = merged_df['Positive_Count'] + merged_df['Negative_Count']
 # Fit the linear regression model for Total_Count
 model_total_count = sm.OLS(y_total_count, X_total_count).fit()
 
-# Save the model to a file within the "flair_regression" folder
-filename_total_count = '_'.join(independent_vars_total_count)
-model_total_count.save(os.path.join('flair_regression', f'total_count_model_{filename_total_count}.pkl'))
+# Save the model to a file within the "flair_regression" folder for prisons only
+filename_total_count_prisons = '_'.join(independent_vars_total_count + ['Prisons'])
+model_total_count.save(os.path.join('flair_regression', f'total_count_model_{filename_total_count_prisons}.pkl'))
 
 # Print the summary of the regression for Total_Count
-print(f"\nLinear Regression for Total_Count using independent variables: {', '.join(independent_vars_total_count)}")
+print(f"\nLinear Regression for Total_Count (Prisons only) using independent variables: {', '.join(independent_vars_total_count)}")
 print(model_total_count.summary())
